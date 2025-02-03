963c566822ab490c780bdbbb08b6d3c173f9cfd8
Oscar <highspeedtumbleweed@gmail.com>
2025-02-03 02:11:58 +0000

    Hello, I Just swapped the 'Batterground' text to the Title icon I made, adjusted its height, and also push the start button from 250 to 260 in height. There may be centering issues with the Icon; I just eyed the position.

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ let buttons = [];
 let gameStarted = false;
 let settingMenu = false;
 let bgImage;
+let titleIcon;
 
 function preload() {
   bgImage = loadImage('assets/baseball_bg.jpg'); 
+  titleIcon = createImg('assets/Title_Logo_2.png','Title_Icon');
 }
 
 function setup() {
@@ -12,7 +14,7 @@ function setup() {
   textAlign(CENTER, CENTER);
   textSize(32);
   
-  buttons.push(new Button("Start Game", width / 2, 250)); 
+  buttons.push(new Button("Start Game", width / 2, 260)); 
   buttons.push(new Button("Settings", width - 75, height - 35, true)); 
   buttons.push(new Button("Credits", width - 150, height - 35, true));
   
@@ -31,7 +33,9 @@ function draw() {
     textSize(64);
     stroke(0);
     
-    text("Batterground", width / 2, 130);
+    titleIcon.size(256,256);
+    titleIcon.position(160 ,-25);
+    //text("Batterground", width / 2, 130); <- Batterground text
     
     textStyle(NORMAL);
     for (let btn of buttons) {
