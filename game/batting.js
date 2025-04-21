9e14f55d0a1575f983f00a45f2fe0bfbfa185046
Keegan Calkins <kcalkin1@kent.edu>
2025-04-21 18:12:45 -0400

    fixed bug for runners forced to run when they shouldnt be

--- game/batting.js
+++ game/batting.js
@@ -67,6 +67,7 @@ function playerHit() {
     batter.running = true;
     runners.forEach(runner => {
         if (bases[runner.base - 1].occupied) {
+            if (runner.base === 3 && !bases[runner.base - 2].occupied)return;
             runner.running = true;
             bases[runner.base].occupied = false;
         }
